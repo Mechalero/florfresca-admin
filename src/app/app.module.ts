3ed64b8572c250e3f0df9fb6c24924449425e7f0
Leider Gaitan <leider@if-cs.com>
2018-10-05 17:29:21 -0500

    Inicio de administrador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './blocks/footer/footer.component';
+import { NavComponent } from './blocks/nav/nav.component';
+import { SidebarComponent } from './blocks/sidebar/sidebar.component';
+import { RoutingModule } from './app.routing';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavComponent,
+    SidebarComponent,
+    DashboardComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
